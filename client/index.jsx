e17f55aa56828b25f4356b5d292c60700dd6377b
tomrarcher <syvertcollege@gmail.com>
2022-02-20 16:53:00 +0100

    Ready for Heroku

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,32 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-ReactDom.render(<h1>Hello World</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie Database</h1>
+      <div>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/register"}>Register new user</Link>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Hello this is login</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDom.render(<Application />, document.getElementById("app"));
