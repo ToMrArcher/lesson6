f2e2bc88cfc506ea1846308a4b9b6248fa968c51
tomrarcher <syvertcollege@gmail.com>
2022-02-20 16:12:17 +0100

    husky

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log("listening on port " + server.address().port);
+  console.log("listening on port " + server.address().port);
 });
