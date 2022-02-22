40581e19bf9f313013b5f2f45a5f59f1d8c94e59
tomrarcher <syvertcollege@gmail.com>
2022-02-22 16:15:33 +0100

    move useloader and fetchjson

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-function FrontPage() {
+function LoginLinks() {
   return (
     <div>
-      <h1>Movie Database</h1>
       <div>
         <Link to={"/login"}>Login</Link>
+      </div>
+      <div>
         <Link to={"/register"}>Register new user</Link>
       </div>
+    </div>
+  );
+}
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  return await res.json();
+}
+
+function useLoader(loadingFn) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+  useEffect(async () => {
+    setLoading(true);
+    try {
+      setData(await loadingFn());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  return { loading, error, data };
+}
+
+function FrontPage() {
+  const { loading, error, data } = useLoader(
+    async () => await fetchJSON("/api/login")
+  );
+  const user = data;
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  return (
+    <div>
+      <h1>My Movie Database Is</h1>
+      {error && (
+        <div style={{ border: "1px solid red", background: "Pink" }}>
+          An error has occured: {error.toString()}
+        </div>
+      )}
+      {user ? <div>{user.fullName}</div> : <LoginLinks />}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
 app.get("/api/login", (req, res) => {
-  res.json({
-    username: "admin",
-    fullName: "Syvert",
-  });
+  function respond() {
+    res.json({
+      username: "admin",
+      fullName: "Syvert",
+    });
+  }
+
+  setTimeout(respond, 3000);
+});
+
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method == "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
